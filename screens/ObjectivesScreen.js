44c2b722c221a679525d45de0946a04bbe83d390
root <25795521+MadanAnand@users.noreply.github.com>
2023-03-19 23:04:24 +0100

    objective screen

--- screens/ObjectivesScreen.js
+++ screens/ObjectivesScreen.js
@@ -1,9 +1,13 @@
 import { View, Text,ImageBackground } from 'react-native'
 import React from 'react'
-import { useNavigation, useRoute } from '@react-navigation/native'
+import {  useNavigation, useRoute } from '@react-navigation/native'
+import { useEffect, useLayoutEffect, useState } from "react";
+import { client } from "../sanity";
 
 const ObjectivesScreen = () => {
   const navigatation = useNavigation()
+  const [currentRelation, setCurrentRelation] = useState([])
+
   //To-Do slicer
   const relationship = null
   
@@ -12,6 +16,29 @@ const ObjectivesScreen = () => {
     title,
   }} =useRoute();
 
+  useEffect(()=> {
+    const params = {searchName: 'Stage 1'};
+    
+    client.fetch(`*[_type == "relationshipStage" && title == $searchName]{
+      title,
+        objective[] -> 
+        {...,} 
+      }`
+    , params).then((relation) => {
+        relation?.map(relation=> (
+            console.log('Fetching name of current relationstage '+ relation.title),
+            //player?.relationship?.map(relation => console.log(relation.title)),
+      
+            setCurrentRelation(relation.objective)
+        ))    
+    });  
+    
+
+    currentRelation?.map(objective => {
+      console.log('`Objectives are ` '+ objective)
+    });
+    },[] );
+    
   return (
     <View>
    <ImageBackground source={require('../images/leafyBackground.png')} style={{width: '100%', height: '100%'}}>
